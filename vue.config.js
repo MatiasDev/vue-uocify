9d882f9e84a692028000163cacb5ff3988adfee1
MatiasDev <matias.bautista@gmail.com>
2020-12-31 11:23:52 +0100

    Practica PWA

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,28 @@ module.exports = {
         prependData: `@import "@/styles/_variables.scss";`
       }
     }
+  },
+  pwa: {
+    name: 'vue-uocify',
+    themeColor: '#73EDFF',
+    msTileColor: '#1F1F89',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: '#1F1F89',
+    assetsVersion:1.0,
+  //manifestPath:
+    iconPaths:{
+        favicon32: 'img/icons/favicon-32x32.png',
+        favicon16: 'img/icons/favicon-16x16.png',
+        appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+        maskIcon: 'img/icons/safari-pinned-tab.svg',
+        msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    },
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    }
   }
 };
