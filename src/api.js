634ee7f410c1d35725a0a00fce51c05426711d90
MatiasDev <matias.bautista@gmail.com>
2021-01-04 16:37:52 +0100

    Build 7 para Netlify2

--- src/api.js
+++ src/api.js
@@ -1,56 +1,29 @@
 export async function getPlaylists() {
-    const res = await fetch("https://api.deezer.com/chart/0/playlists",
-    {
-        mode: 'cors', // no-cors, *cors, same-origin
-        headers: {
-            'Access-Control-Allow-Origin':'*'
-          }
-    })
+    const res = await fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/playlists")
     const data = await res.json();
     return data;
 }
 
 export async function getPlaylist(idplaylist) {
-    const res = await fetch("https://api.deezer.com/playlist/"+idplaylist,
-    {
-        mode: 'cors', // no-cors, *cors, same-origin
-        headers: {
-            'Access-Control-Allow-Origin':'*'
-          }
-    })
+    const res = await fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/playlist/"+idplaylist)
     const data = await res.json();
     return data;
 }
 
 export async function getTracks(q) {
-    const res = await fetch("https://api.deezer.com/search/track?q="+q,
-    {
-        mode: 'cors', // no-cors, *cors, same-origin
-        headers: {
-            'Access-Control-Allow-Origin':'*'
-          }
-    })
+    const res = await fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/track?q="+q)
     const data = await res.json();
     return data;
 }
 
 export async function getAlbums(q) {
-    const res = await fetch("https://api.deezer.com/search/album?q="+q,
-    {
-        mode: 'cors', // no-cors, *cors, same-origin
-        headers: {
-            'Access-Control-Allow-Origin':'*'
-          }
-    })
+    const res = await fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/album?q="+q)
     const data = await res.json();
     return data;
 }
 
 export async function getArtists(q) {
-    const res = await fetch("https://api.deezer.com/search/artist?q="+q,
-    {
-        mode: 'cors', // no-cors, *cors, same-origin
-    })
+    const res = await fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/artist?q="+q)
     const data = await res.json();
     return data;
 }
