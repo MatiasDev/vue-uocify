5424313f06a720e0dc8445d03454306dfb8dd781
MatiasDev <matias.bautista@gmail.com>
2021-01-04 16:22:54 +0100

    Build 5 para Netlify2

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 export async function getPlaylists() {
-    const res = await fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/playlists",
+    const res = await fetch("https://api.deezer.com/chart/0/playlists",
     {
         mode: 'no-cors', // no-cors, *cors, same-origin
     })
@@ -8,25 +8,37 @@ export async function getPlaylists() {
 }
 
 export async function getPlaylist(idplaylist) {
-    const res = await fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/playlist/"+idplaylist)
+    const res = await fetch("https://api.deezer.com/playlist/"+idplaylist,
+    {
+        mode: 'no-cors', // no-cors, *cors, same-origin
+    })
     const data = await res.json();
     return data;
 }
 
 export async function getTracks(q) {
-    const res = await fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/track?q="+q)
+    const res = await fetch("https://api.deezer.com/search/track?q="+q,
+    {
+        mode: 'no-cors', // no-cors, *cors, same-origin
+    })
     const data = await res.json();
     return data;
 }
 
 export async function getAlbums(q) {
-    const res = await fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/album?q="+q)
+    const res = await fetch("https://api.deezer.com/search/album?q="+q,
+    {
+        mode: 'no-cors', // no-cors, *cors, same-origin
+    })
     const data = await res.json();
     return data;
 }
 
 export async function getArtists(q) {
-    const res = await fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/search/artist?q="+q)
+    const res = await fetch("https://api.deezer.com/search/artist?q="+q,
+    {
+        mode: 'no-cors', // no-cors, *cors, same-origin
+    })
     const data = await res.json();
     return data;
 }
